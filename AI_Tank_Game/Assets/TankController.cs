79a0659739930bd9ac2105aa17e762780a6653ed
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-10-26 09:37:33 +0100

    Based structure

---
+++ AI_Tank_Game/Assets/TankController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(TankInputs))]
+public class TankController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private TankInputs input;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        input = GetComponent<TankInputs>();
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
