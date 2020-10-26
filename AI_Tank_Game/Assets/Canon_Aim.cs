6ed7c8c2a7463a7d3ac08ba8ee0b0fc283c9d4d1
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-10-26 10:24:31 +0100

    Cannon aim

---
+++ AI_Tank_Game/Assets/Canon_Aim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Canon_Aim : MonoBehaviour
+{
+    public Transform target;
+    public Transform cannon;
+
+    private void Update()
+    {
+        Vector3 targetPostition = new Vector3(target.position.x,
+                                               cannon.transform.position.y,
+                                               target.position.z);
+        cannon.transform.LookAt(targetPostition);
+    }
+
+}
