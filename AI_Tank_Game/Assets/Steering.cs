acdcc14c8b91eb3d0fe6b8c3d0461b809274ee33
Sanmopre <40120264+Sanmopre@users.noreply.github.com>
2020-11-03 14:26:03 +0100

    Steering

---
+++ AI_Tank_Game/Assets/Steering.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steering : MonoBehaviour
+{
+
+    public GameObject target;
+    public int stopDistance;
+
+    float turnSpeed;
+    float movSpeed;
+    public Vector3 movement;
+
+    public float turnAcceleration;
+    public float maxTurnSpeed;
+    public float acceleration;
+    public float maxSpeed;
+    public Quaternion rotation;
+
+    void Update()
+    {
+        if (Vector3.Distance(target.transform.position, transform.position) <
+    stopDistance) return;
+        Seek();   // calls to this function should be reduced
+        turnSpeed += turnAcceleration * Time.deltaTime;
+        turnSpeed = Mathf.Min(turnSpeed, maxTurnSpeed);
+        movSpeed += acceleration * Time.deltaTime;
+        movSpeed = Mathf.Min(movSpeed, maxSpeed);
+        transform.rotation = Quaternion.Slerp(transform.rotation,
+                                              rotation, Time.deltaTime * turnSpeed);
+        transform.position += transform.forward.normalized * movSpeed *
+                              Time.deltaTime;
+    }
+
+    void Seek() {
+        Vector3 direction = target.transform.position - transform.position;
+        direction.y = 0f;
+        movement = direction.normalized * acceleration;
+        float angle = Mathf.Rad2Deg * Mathf.Atan2(movement.x, movement.z);
+        rotation = Quaternion.AngleAxis(angle, Vector3.up);
+    }
+}
